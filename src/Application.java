d55520564207084e8bd7db7eb1ce3ddd405fac48
jonaswahanik <wahanik.jonas@vashonsd.org>
2021-10-05 10:48:27 -0700

    first commit

---
+++ src/Application.java
@@ -0,0 +1,9 @@
+public class Application {
+    public static void main(String[] args) {
+        System.out.println(squareOf(4));
+    }
+
+    public static int squareOf(int n) {
+        return n * n;
+    }
+}
